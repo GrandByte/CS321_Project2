e92d39cc810b6c0dbb5426334fa484b463512f23
Ctrl-Dalt-Del <61924809+Ctrl-Dalt-Del@users.noreply.github.com>
2020-03-09 22:32:00 -0500

    More minor changes
    Fixed more typos. I'd rather catch them now than after it gives an error

--- src/cs321_Project2/Jester.java
+++ src/cs321_Project2/Jester.java
@@ -20,7 +20,7 @@ public class Jester {
 		if((Integer.compare(F1.strength, F2.strength)+ Integer.compare(F1.reach, F2.reach) + Integer.compare(F1.speed, F2.speed)) > 0)
 		{
 			System.out.println("Alright folks! It looks like this match is going to be a doozy. "
-					+ "I think we're gonna see " + F1.name + " Take home a win for today, with their");
+					+ "I think we're gonna see " + F1.Name + " Take home a win for today, with their");
 			if(Integer.compare(F1.strength, F2.strength) > 0)
 			{
 				System.out.println("Mighty Strength,");
@@ -34,12 +34,12 @@ public class Jester {
 				System.out.println("Astonishing Speed,");
 			}
 			System.out.println("and Charming Smile! Good luck and good fight everyone!");
-			winbet = F1.name;
+			winbet = F1.Name;
 		}
 		else if((Integer.compare(F1.strength, F2.strength)+ Integer.compare(F1.reach, F2.reach) + Integer.compare(F1.speed, F2.speed)) < 0)
 		{
 			System.out.println("Alright folks! It looks like this match is going to be a doozy. "
-					+ "I think we're gonna see " + F2.name + " Take home a win for today, with their");
+					+ "I think we're gonna see " + F2.Name + " Take home a win for today, with their");
 			if(Integer.compare(F1.strength, F2.strength) < 0)
 			{
 				System.out.println("Mighty Strength,");
@@ -53,7 +53,7 @@ public class Jester {
 				System.out.println("Astonishing Speed,");
 			}
 			System.out.println("and Charming Smile! Good luck and good fight everyone!");
-			winbet = F2.name;
+			winbet = F2.Name;
 		}
 	}
 	
@@ -62,20 +62,20 @@ public class Jester {
 	*/
 	void CommentOnMiddle(Fighter F)
 	{
-		System.out.println("Wow! " + F.name + " is looking rough, seems like they're already halfway in their grave!");
+		System.out.println("Wow! " + F.Name + " is looking rough, seems like they're already halfway in their grave!");
 	}
 	/**
 	*Takes one fighter argument, and prints out a reactionary statement after a comparison to check if the winner of the match was the previously predicted victor.
 	*/
 	void CommentOnEnd(Fighter winner)
 	{
-		if (winner.name == winbet)
+		if (winner.Name == winbet)
 		{
-			System.out.println("How about that, " + winner.name + "won! Looks like these eyes are still as sharp as my wit.");
+			System.out.println("How about that, " + winner.Name + "won! Looks like these eyes are still as sharp as my wit.");
 		}
 		else
 		{
-			System.out.println("Well, that's certainly a surprise. It looks like " + winner.name + " is the victor, against all odds! Everyone loves an underdog story, don't they?");
+			System.out.println("Well, that's certainly a surprise. It looks like " + winner.Name + " is the victor, against all odds! Everyone loves an underdog story, don't they?");
 		}
 	}
 		
