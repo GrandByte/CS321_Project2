9610f043ee7f619633a7af6467c684e6b4a8ad9e
Ctrl-Dalt-Del <61924809+Ctrl-Dalt-Del@users.noreply.github.com>
2020-03-10 22:56:11 -0500

    Adding what I've got

---
+++ cs321_Project2 - Copy/MenuTest.java
@@ -0,0 +1,14 @@
+package cs321_Project2;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MenuTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
